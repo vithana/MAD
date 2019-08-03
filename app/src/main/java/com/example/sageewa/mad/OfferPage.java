94f795ba0daf0e6070e3336741c6afd230334a1c
vithana <sajeewa998@gmail.com>
2019-08-03 22:39:22 +0530

    just add dummy button to home

---
+++ app/src/main/java/com/example/sageewa/mad/OfferPage.java
@@ -0,0 +1,17 @@
+package com.example.sageewa.mad;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class OfferPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_offer_page);
+
+        Intent intent = getIntent();
+
+    }
+}
